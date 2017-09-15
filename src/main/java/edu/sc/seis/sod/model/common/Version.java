1def90ac7c030c9be2d4fdd500108cf27b044722
Philip Crotwell <crotwell@seis.sc.edu>
2017-09-15 12:13:23 -0400

    add equals/hashcode

--- src/main/java/edu/sc/seis/sod/model/common/Version.java
+++ src/main/java/edu/sc/seis/sod/model/common/Version.java
@@ -43,6 +43,38 @@ public class Version{
         this.schemaChange = schemaChange;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + dbid;
+        result = prime * result + (schemaChange ? 1231 : 1237);
+        result = prime * result + ((version == null) ? 0 : version.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Version other = (Version) obj;
+        if (dbid != other.dbid)
+            return false;
+        if (schemaChange != other.schemaChange)
+            return false;
+        if (version == null) {
+            if (other.version != null)
+                return false;
+        } else if (!version.equals(other.version))
+            return false;
+        return true;
+    }
+
+    
 }
 
 
