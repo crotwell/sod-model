1e5bc21c0a473269774dcd303b75bad89aec0721
Philip Crotwell <crotwell@seis.sc.edu>
2021-05-10 14:01:42 -0400

    new seisFile,seedcodec

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,8 @@ version = "4.0.0-SNAPSHOT"
 plugins {
     // Apply the java-library plugin to add support for Java Library
     `java-library`
-    eclipse
+    `eclipse`
+    `maven-publish`
 }
 
 
@@ -19,8 +20,8 @@ java {
 
 dependencies {
   implementation("org.slf4j:slf4j-api:1.7.30")
-  implementation("edu.sc.seis:seedCodec:1.0.11")
-  implementation("edu.sc.seis:seisFile:1.7.4")
+  implementation("edu.sc.seis:seedCodec:1.1.1")
+  implementation("edu.sc.seis:seisFile:2.0.0")
   implementation("org.json:json:20140107")
   // Use JUnit Jupiter API for testing.
   testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.1")
@@ -40,3 +41,15 @@ configurations.all {
 repositories {
   mavenCentral()
 }
+
+
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+}
+
+
